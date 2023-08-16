e875a546644def9af02a7509ae2966b87358d325
line <line.xj.lin@gmail.com>
2023-08-16 14:48:15 +0800

    add mac safari added

--- webview/webview_nonwindows.go
+++ webview/webview_nonwindows.go
@@ -3,37 +3,33 @@
 
 package webview
 
-/*
-#cgo linux pkg-config: webkit2gtk-4.0
-#cgo darwin CFLAGS: -DDARWIN -x objective-c -fobjc-arc
-#cgo darwin LDFLAGS: -framework Cocoa -framework Webkit
-
-#include "webview.h"
-*/
-import "C"
-
 import (
+	"log"
+	"os/exec"
+	"runtime"
+
 	"github.com/getlantern/systray"
 	icon "github.com/linexjlin/systray-icons/openai-logomark"
 )
 
-func configureWebview(title string, width, height int) {
-	C.configureAppWindow(C.CString(title), C.int(width), C.int(height))
-}
+var URL string
 
 func openUrl(url string) {
-	configureWebview("ChatDesk", 1024, 768)
-	C.showAppWindow(C.CString(url))
+	var cmd *exec.Cmd
+	if runtime.GOOS == "darwin" {
+		cmd = exec.Command("open", "-a", "Safari", url)
+	} else {
+		cmd = exec.Command("xdg-open", url)
+	}
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
-
-var URL = ""
-var W = 1024
-var H = 768
 
 func ShowWebview(url string) {
 	URL = url
-	systray.Register(onReady, nil)
-	configureWebview("ChatDesk", 1024, 768)
+	systray.Run(onReady, nil)
 }
 
 func onReady() {
